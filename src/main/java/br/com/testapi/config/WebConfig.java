4ee9ab3ed60229bbc68ec3358d6d7dad5de6a64e
Jonas Lopes <jmdlopes.almeida@gmail.com>
2021-03-25 17:39:49 +0000

    adding support for yaml

--- src/main/java/br/com/testapi/config/WebConfig.java
+++ src/main/java/br/com/testapi/config/WebConfig.java
@@ -1,25 +1,62 @@
 package br.com.testapi.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import br.com.testapi.serialization.converter.YmlJackson2HttpMessageConverter;
+
 @Configuration
 @EnableWebMvc
-public class WebConfig implements WebMvcConfigurer{
-	
+public class WebConfig implements WebMvcConfigurer {
+
+	private static final MediaType MEDIA_TYPE_YML = MediaType.valueOf("application/x-yaml");
+
+	@Override
+	public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+		converters.add(new YmlJackson2HttpMessageConverter());
+	}
+
 	@Override
 	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-		configurer.favorParameter(false)
-		.ignoreAcceptHeader(false)
-		//default content JSON
-		.defaultContentType(MediaType.APPLICATION_JSON)
-		.mediaType("json", MediaType.APPLICATION_JSON)
-		.mediaType("xml", MediaType.APPLICATION_XML);
-	
-	
+
+		// ### Via EXTENSION EX: .xml .json .x-yaml ###
+		// http://localhost:8080/api/person/v1/.xml
+		 configurer.favorParameter(false) 
+		 .ignoreAcceptHeader(false)
+		 .defaultContentType(MediaType.APPLICATION_JSON) 
+		 .mediaType("json", MediaType.APPLICATION_JSON) 
+		 .mediaType("xml", MediaType.APPLICATION_XML)
+		 .mediaType("x-yaml", MEDIA_TYPE_YML);
+		 
+
+		// ### Via QUERY PARAM Ex: ?mediaType=xml ###
+
+		// http://localhost:8080/api/person/v1/1?mediaType=xml
+		/*
+		 * configurer .favorParameter(true) .parameterName("mediaType")
+		 * .ignoreAcceptHeader(true) .useRegisteredExtensionsOnly(false)
+		 * .defaultContentType(MediaType.APPLICATION_JSON) .mediaType("json",
+		 * MediaType.APPLICATION_JSON) .mediaType("xml", MediaType.APPLICATION_XML)
+		 * .mediaType("x-yaml", MEDIA_TYPE_YML);
+		 */
+
+		// ### Via Header Ex: Accept application/xml or application/json ###
+		
+		/*
+		 * configurer .favorParameter(false) .ignoreAcceptHeader(false)
+		 * .useRegisteredExtensionsOnly(false)
+		 * .defaultContentType(MediaType.APPLICATION_JSON) .mediaType("json",
+		 * MediaType.APPLICATION_JSON) .mediaType("xml", MediaType.APPLICATION_XML)
+		 * .mediaType("x-yaml", MEDIA_TYPE_YML);
+		 */
+		 
+
 	}
 
 }
